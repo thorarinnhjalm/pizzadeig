5ba18c5974ee5259cd2a061093d79debdc45acba
Thorarinn Hjalmarsson <thorarinnhjalmarsson@gmail.com>
2026-03-27 14:41:05 +0000

    update: Use uploaded dough image on the front page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -84,7 +84,7 @@ export default async function HomePage({ params }: { params: Promise<{ locale: s
                 <div className="w-20 h-20 rounded-xl overflow-hidden">
                   {/* eslint-disable-next-line @next/next/no-img-element */}
                   <img 
-                    src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?q=80&w=400" 
+                    src="/images/dough-frontpage.jpg" 
                     alt="Pizza Dough" 
                     className="w-full h-full object-cover"
                   />
